b1338d88dd2c5bddf721b9fdb8f22686a0fceffe
kangleng <kangleng@gmail.com>
2021-11-24 11:45:26 +0800

    Refactor checking of password into a function

--- password_example.py
+++ password_example.py
@@ -8,10 +8,14 @@ Password Checker. Created by Owen Grady. 24-11-2021
 def main():
     """Start Program."""
     password = "12345678"
-    if password == "12345678":
+    if check_password(password):
         print("Logged in")
     else:
         print("Illegal access")
+
+
+def check_password(password):
+    return password == "12345678"
 
 
 main()

1c3eb4069603267536f9f9653b6a6fa8006dca6d
kangleng <kangleng@gmail.com>
2021-11-24 11:38:25 +0800

    Add password checking function

---
+++ password_example.py
@@ -0,0 +1,17 @@
+"""
+Check Login Credentials.
+
+Password Checker. Created by Owen Grady. 24-11-2021
+"""
+
+
+def main():
+    """Start Program."""
+    password = "12345678"
+    if password == "12345678":
+        print("Logged in")
+    else:
+        print("Illegal access")
+
+
+main()
